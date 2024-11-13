def821f6b6bdd93793c9a31644416cb7d06e128f
JPLaham <jplaham@protonmail.com>
2024-11-12 23:56:37 -0400

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-// Use the n8nChat namespace to access createChat
-n8nChat.createChat({
-    webhookUrl: 'https://sofrito.app.n8n.cloud/webhook/chat', // Ensure this matches your webhook URL
-    parentElementSelector: '#n8n-chat'
-});
